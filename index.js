240dd635eac97c6e1feb2607228a50750be2ef4a
mpower17 <maxim.zaykov97@gmail.com>
2021-12-14 19:35:39 +0300

    исправляет ошибку

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import {createReadStream} from 'fs';
 import crypto from 'crypto';
 import http from 'http';
 import mongoose from "mongoose";
+import { writeFileSync } from 'fs';
 
 import appSrc from './app.js'
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongoose);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongoose, writeFileSync);
 
 app.listen(process.env.PORT);
